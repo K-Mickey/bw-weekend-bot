67da782288e54117d487985407f75c5f00685e5b
Mikhail Kokuev <kokmickey@yandex.ru>
2026-05-28 16:14:39 +0500

    remove old version

--- old/content/__init__.py
+++
@@ -1,8 +0,0 @@
-from .location import LOCATION_MESSAGES
-from .main_menu import MENU_MESSAGES
-from .schedule import SCHEDULE_MESSAGES
-from .states import States
-
-MESSAGES = MENU_MESSAGES | LOCATION_MESSAGES | SCHEDULE_MESSAGES
-
-__all__ = ["States", "MESSAGES"]
