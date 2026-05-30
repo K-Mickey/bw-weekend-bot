84f063320a5f2def6acff2cced3df9d4f4bb3dfc
Mikhail Kokuev <kokmickey@yandex.ru>
2026-05-30 15:43:16 +0500

    Optimize `CacheKey` creation with factory method for media nodes.

--- src/infrastructure/file_cache/value_objects/cache_key.py
+++ src/infrastructure/file_cache/value_objects/cache_key.py
@@ -1,5 +1,6 @@
-from typing import NamedTuple
+from typing import NamedTuple, Self
 
+from src.domain.entities.media import PhotoNode, VideoNode
 from src.domain.value_objects.network import Network
 from src.infrastructure.file_cache.value_objects.cache_media_type import CacheMediaType
 
@@ -11,3 +12,19 @@ class CacheKey(NamedTuple):
 
     def __str__(self) -> str:
         return f"{self.media_type}/{self.network}/{self.key}"
+
+    @classmethod
+    def create(cls, media: PhotoNode | VideoNode, network: Network) -> Self:
+        match media:
+            case PhotoNode():
+                media_type = CacheMediaType.PHOTO
+            case VideoNode():
+                media_type = CacheMediaType.VIDEO
+            case _:
+                raise ValueError(f"Unsupported media type: {media}")
+
+        return cls(
+            media_type=media_type,
+            network=network,
+            key=media.url,
+        )
